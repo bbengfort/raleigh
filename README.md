147cf0e1913cd9e86a64aa693cb66691cf76139a
Benjamin Bengfort <benjamin@bengfort.com>
2017-04-17 13:30:07 -0400

    centrality computation

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# raleigh
-graphs for pydata carolinas
+# Raleigh
+
+This repository contains scripts, notebooks, utilities, and data for graph analysis and image generation. Most of the images drawn in this repo were used for the PyData Carolinas talk on dynamic graphs. Others were used to draw images or perform computations for _Applied Text Analysis with Python_.
+
+Note that this repository requires [graph tool](https://graph-tool.skewed.de/).
+
+Contents:
+
+1. data: contains compressed graphs and data in a variety of formats.
+2. images: contains output from scripts and analysis
+3. notebooks: contains Jupyter notebooks of graph analyses
+4. raleigh: contains common code to be imported by other utilities
+5. scripts: contains long running scripts to be executed. 
--- scripts/centrality.py
+++ scripts/centrality.py
@@ -1,16 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
+import sys
+import time
 import heapq
+import argparse
 import networkx as nx
 
 from operator import itemgetter
+
+sys.path.append("..")
+from raleigh.notify import notify
+
 
 def nbest_centrality(G, metric, n=10, attr="centrality", **kwargs):
     # Compute the centrality scores for each vertex
     scores = metric(G, **kwargs)
 
     # Set the score as a property on each node
-    nx.set_node_attributes(G, attr, scores)
+    # nx.set_node_attributes(G, attr, scores)
 
     # Filter scores (do not include in book)
     ntypes = nx.get_node_attributes(G, 'type')
@@ -21,12 +28,43 @@ def nbest_centrality(G, metric, n=10, attr="centrality", **kwargs):
 
     # Find the top n scores and print them along with their index
     topn = heapq.nlargest(n, phrases, key=itemgetter(1))
+    output = []
     for idx, item in enumerate(topn):
-        print("{}. {}: {:0.4f}".format(idx+1, *item))
+        output.append("{}. {}: {:0.4f}".format(idx+1, *item))
 
-    return G
+    return "\n".join(output)
+
+
+def compute_centrality(graphml, metric, n=25, recipient=None):
+    start = time.time()
+    metric_func = {
+        "degree": nx.degree_centrality,
+        "betweenness": nx.betweenness_centrality,
+        "closeness": nx.closeness_centrality,
+        "eigenvector": nx.eigenvector_centrality,
+        "katz": nx.katz_centrality,
+    }[metric]
+
+    G = nx.read_graphml(graphml)
+    output = nbest_centrality(G, metric_func, n)
+    delta = time.time() - start
+
+    if recipient:
+        subject = "{} centrality computation finished".format(metric)
+        message = output + "\n\nFinished in {:0.3f} seconds".format(delta)
+        notify(recipient, subject, message)
+
+    else:
+        print(output)
+        print("\nFinished in {:0.3f} seconds".format(delta))
 
 
 if __name__ == '__main__':
-    G = nx.read_graphml("phrases.graphml")
-    nbest_centrality(G, nx.closeness_centrality, 25, "closeness", normalized=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--metric', help='metric to compute')
+    parser.add_argument('-n', '--notify', help='email address to notify on complete')
+    parser.add_argument('-t', '--topn', type=int, help='fetch top n results')
+    parser.add_argument('graph', nargs=1, help='location on disk of graphml')
+    args = parser.parse_args()
+
+    compute_centrality(args.graph[0], args.metric, args.topn, args.notify)
